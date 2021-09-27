01f98f206c32ef46ea1c88239f26df60e04b1e67
Boris Alarcon <boris.alarco@gmail.com>
2021-09-27 18:53:18 -0300

    Solucionado el problema de CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const myconn = require("express-myconnection");
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 app.set('port', process.env.PORT || 3002)
@@ -16,6 +17,7 @@ const dbOptions = {
 //middlewares ----------------
 app.use(myconn(mysql, dbOptions, 'single'))
 app.use(express.json());
+app.use(cors());
 
 //-- Routes-----------------
 app.get('/',(req, res) =>{
